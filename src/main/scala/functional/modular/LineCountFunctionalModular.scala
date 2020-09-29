971207ac6777e3bcb1f711115aad97f221f4706a
Konstantin Läufer <klaeufer@gmail.com>
2020-09-28 23:45:32 -0500

    added support for command-line args in functional modular examples
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/main/scala/functional/modular/LineCountFunctionalModular.scala
+++ src/main/scala/functional/modular/LineCountFunctionalModular.scala
@@ -3,9 +3,12 @@ package functional.modular
 /** Reads lines and prints line count along with line itself. */
 object LineCountFunctionalModular extends App {
 
-  runWithStdIO(run)
+  runWithStdIO(run, args)
 
-  def run(lines: Iterator[String]): Iterator[(Int, String)] = {
+  def run(lines: Iterator[String], args: Array[String] = Array.empty): Iterator[(Int, String)] = {
+    if (args.length > 0) {
+      System.err.println("args: " + args.toSeq)
+    }
     val counts = Iterator.from(1)
     counts.zip(lines)
   }
